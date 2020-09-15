c1c41fe0c13ea023173a464796443498356697d7
MINHO CHOI <minhocalgary@gmail.com>
2020-09-15 13:17:46 -0600

    finished dice roller

--- HelloWorld/app/src/main/java/com/example/helloworld/MainActivity.kt
+++ HelloWorld/app/src/main/java/com/example/helloworld/MainActivity.kt
@@ -3,7 +3,9 @@ package com.example.helloworld
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
 import android.widget.Toast
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -12,10 +14,42 @@ class MainActivity : AppCompatActivity() {
 
         val rollButton: Button = findViewById(R.id.roll_button)
         rollButton.setOnClickListener { rollDice() }
+
+        val countButton: Button = findViewById((R.id.add_button))
+        countButton.setOnClickListener{ countUp() }
     }
 
     private fun rollDice() {
-        Toast.makeText(this, "button clicked",
-            Toast.LENGTH_SHORT).show()
+        val resultText: TextView = findViewById(R.id.result_text)
+
+        var randomInt = (1..6).random()
+
+        resultText.text = randomInt.toString()
+
+        //   Toast.makeText(this, "button clicked",
+        //      Toast.LENGTH_SHORT).show()
+    }
+
+    private fun countUp() {
+        val resultText: TextView = findViewById(R.id.result_text)
+        val text = resultText.text
+
+        // If text is the default "Hello World!" set that text to 1.
+        if (text != "Hello World!"){
+            // Otherwise, increment the number up to 6.
+            // The text value in resultText.text is an instance of the CharSequence class;
+            // it needs to be converted to a String object before it can be converted to an int.
+            var resultInt = resultText.text.toString().toInt()
+
+            if (resultInt < 6) {
+                resultInt++
+                resultText.text = resultInt.toString()
+            }
+        }
+        else {
+            resultText.text = "Hello World!"
+        }
     }
 }
+
+
