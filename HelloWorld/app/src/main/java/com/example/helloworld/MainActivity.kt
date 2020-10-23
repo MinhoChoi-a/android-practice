4747f544750133d37c08f789844567ac5f2da3ed
MINHO CHOI <minhocalgary@gmail.com>
2020-10-23 14:51:32 -0600

    fixed mainActivity for about button

--- HelloWorld/app/src/main/java/com/example/helloworld/MainActivity.kt
+++ HelloWorld/app/src/main/java/com/example/helloworld/MainActivity.kt
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_main.*
 import android.content.Intent
+import android.view.Menu
+import android.view.MenuItem
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -14,5 +16,18 @@ class MainActivity : AppCompatActivity() {
             val intent = Intent(this, DiceActivity::class.java)
             startActivity(intent)
         }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if(item?.itemId == R.id.action_about) {
+            AboutActivity.start(this)
+            return true
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
