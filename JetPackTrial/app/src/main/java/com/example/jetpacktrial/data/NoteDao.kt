445cc8bbea63d8506fd35579373ab0b6dd5b3cf8
MINHO CHOI <minhocalgary@gmail.com>
2020-11-14 10:27:01 -0700

    set data access object

---
+++ JetPackTrial/app/src/main/java/com/example/jetpacktrial/data/NoteDao.kt
@@ -0,0 +1,26 @@
+package com.example.jetpacktrial.data
+
+import androidx.lifecycle.LiveData
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+
+//Data access object
+@Dao
+interface NoteDao {
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun insertNote(note: NoteEntity)
+
+    //if the bunch of notes already exist, throw away the new ones
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    fun insertAll(notes: List<NoteEntity>)
+
+    //LiveData is kind of observer, so the data is updated automatically
+    @Query("SELECT * FROM notes ORDER BY date ASC")
+    fun getAll(): LiveData<List<NoteEntity>>
+
+    @Query("SELECT * FROM notes WHERE id = :id")
+    fun getNoteById(id: Int): NoteEntity? //it is nullable
+}
