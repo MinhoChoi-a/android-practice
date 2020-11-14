4b1f2195025a219c2e84fc729241ba35427ccff2
MINHO CHOI <minhocalgary@gmail.com>
2020-11-14 10:19:50 -0700

    set room library

--- JetPackTrial/app/src/main/java/com/example/jetpacktrial/data/NoteEntity.kt
+++ JetPackTrial/app/src/main/java/com/example/jetpacktrial/data/NoteEntity.kt
@@ -1,12 +1,18 @@
 package com.example.jetpacktrial.data
 
+import androidx.room.Entity
+import androidx.room.PrimaryKey
 import com.example.jetpacktrial.NEW_NOTE_ID
 import java.util.*
 
 //data means that class is going to have some properties
 
 //data object
+
+@Entity(tableName = "notes")
 data class NoteEntity(
+    //SQLLite will generate unique numeric value
+    @PrimaryKey(autoGenerate = true)
     var id:Int,
     var date: Date,
     var text: String
